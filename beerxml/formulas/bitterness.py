600d29f59539b6c2af2246a1111aa7f420021f68
Rolf Håvard Blindheim <rhblind@gmail.com>
2012-08-28 21:47:14 +0200

    Implemented methods for various bitterness equations

--- beerxml/formulas/bitterness.py
+++ beerxml/formulas/bitterness.py
@@ -6,33 +6,39 @@
 #
 # Resources:
 # http://www.realbeer.com/hops/
+# http://www.realbeer.com/hops/FAQ.html
 # https://docs.google.com/viewer?url=http://www.nthba.org/www/docs/Brew%2520Day%2520Presentation%2520-%2520Hop%2520Bittering.ppt&pli=1
-#
+
+import math
 
 class Tinseth:
     """
-    Estimating IBUs Tinseth Formula
+    Estimating IBUs using Tinseth's equations.
     """
     def ibu(self, alpha_acid_utilization, mg_alpha_acids):
         """
-        IBUs = decimal alpha acid utilization * mg/l of added alpha acids
+        IBUs = decimal alpha acid utilization * mg/l of added alpha acids.
         """
         return  alpha_acid_utilization * mg_alpha_acids
     
-    def mg_alpha_acids(self, alpha_acid_rating, grams_of_hop, batch_size):
+    def mg_alpha_acids(self, alpha_acid_rating, grams_of_hop, batch_size_liters):
         """
-        Calculate the concentration of alpha acids you add to the wort.
-        
-        mg per litre of added alpha acids = decimal AA rating * grams hops * 1000
-                                            -------------------------------------
-                                              volume of finished beer in liters
+        Calculate the concentration of alpha acids you add to the wort using
+        metric system.
         """
-        return alpha_acid_rating * grams_of_hop * 1000 / batch_size
+        return alpha_acid_rating * grams_of_hop * 1000 / batch_size_liters
+    
+    def mg_alpha_acids_non_metric(self, alpha_acid_rating, ounces_of_hop, 
+                                  batch_size_gallons):
+        """
+        Calculate the concentration of alpha acids you add to the wort using
+        non-metric system.
+        """
+        return alpha_acid_rating * ounces_of_hop * 7490 / batch_size_gallons
     
     def alpha_acid_utilization(self, bigness_factor, boil_time_factor):
         """
         Calculate alpha acid utilization.
-        decimal alpha acid utilization = Bigness factor * Boil Time factor
         """ 
         return bigness_factor * boil_time_factor
     
@@ -41,56 +47,111 @@ class Tinseth:
         The Bigness factor accounts for reduced utilization due to 
         higher wort gravities. Use an average gravity value for the 
         entire boil to account for changes in the wort volume.
-        
-        Bigness factor = 1.65 * 0.000125^(wort gravity - 1)
         """
         return 1.65 * 0.000125 ** (float(wort_gravity) -1.0)
     
-    def boil_time_factor(self, time_in_mins):
+    def boil_time_factor(self, time_in_minutes):
         """
-        The Boil Time factor accounts for the change in utilization due to boil time:
-        
-        Boil Time factor = 1 - e^(-0.04 * time in mins)
-                           --------------------------
-                                       4.15
+        The boil time factor accounts for the change in utilization due 
+        to boil time.
         """
         e = 2.71828182846
-        return 1 - e ** (-0.04 * float(time_in_mins)) / 4.15
+        return 1 - e ** (-0.04 * float(time_in_minutes)) / 4.15
 
 
 class Rager:
     """
-    Estimating IBUs Rager Formula
-    
-    IBU = %U * Woz * %A * 7462
-          --------------------
-            Vgal * (1 + GA)
-            
-    Where:
-        GA = (GB – 1.050)/.2
-    
-    GB = Gravity of boiling wort at end of boil
-    GA = Gravity adjustment
-    Woz = Hop weight in ounces
-    Vgal = Volume of beer fermented in gallons
-    %U = Boil utilization percent
-    %A = Hop alpha acid percent
+    Estimate IBUs using Rager's equations.
     """
-    pass
-
-class Garetz:
-    """
-    Estimating IBUs Garetz Formula
     
-    IBU =    %U * Woz * %A * 7490
-          --------------------------
-           Vgal * (1 + GA) * TF * HF
-    
-    Where:
-        GA = (GB – 1.050)/.2
+    def ibu(self, grams_of_hop, utilization_percentage, alpha_acid_percentage,
+            batch_size_liters, gravity_adjustment):
+        """
+        Calculate IBU using metric units.
+        """
+        return grams_of_hop * utilization_percentage * alpha_acid_percentage \
+                * 1000 / batch_size_liters * (1 + gravity_adjustment)
+                
+    def ibu_non_metric(self, ounces_of_hop, utilization_percentage, 
+               alpha_acid_percentage, batch_size_gallons, gravity_adjustment):
+        """
+        Calculate IBU using non-metric units.
+        """
+        return ounces_of_hop * utilization_percentage * alpha_acid_percentage \
+                * 7462 / batch_size_gallons * (1 + gravity_adjustment)
         
-    TF = ((Elevation in feet/550)*0.02) + 1
-    CF = (Ferment Vol)/(Boil Vol)
-    HF = (CF*IBU/260) + 1
+    def utilization_percentage(self, time_in_minutes):
+        """
+        Calculate the alpha acid utilization percentage.
+        """
+        return 18.11 + 13.86 * math.tanh((time_in_minutes - 31.32) / 18.27)
+    
+    def gravity_adjustment(self, boil_gravity):
+        """
+        According to Rager, if the gravity of the boil exceeds 1.050, 
+        there is a gravity adjustment (GA) to factor in.
+        """
+        return (boil_gravity - 1.050) / 0.2 if boil_gravity > 1.050 else 0
+    
+    
+class Garetz:    
     """
-    pass
+    Estimate IBUs using Garetz' equations.
+    """
+    
+    def ibu(self, grams_of_hop, utilization_percentage, alpha_acid_percentage, 
+            batch_size_liters, combined_adjustments):
+        """
+        Calculate IBUs using metric units.
+        """
+        return grams_of_hop * utilization_percentage * alpha_acid_percentage \
+                * 0.1 / batch_size_liters * combined_adjustments
+    
+    def ibu_non_metric(self, ounces_of_hop, utilization_percentage, 
+               alpha_acid_percentage, batch_size_gallons, combined_adjustments):
+        """
+        Calculate IBUs using non-metric units.
+        """
+        return ounces_of_hop * utilization_percentage * alpha_acid_percentage \
+                * 0.749 / batch_size_gallons * combined_adjustments
+    
+    def combined_adjustments(self, gravity_factor, hopping_rate_factor, 
+                             temperature_factor):
+        """
+        According to Garetz, there are several adjustment factors, 
+        that he brings together in the formula with the term 
+        "combined adjustments" (CA)
+        """
+        return gravity_factor * hopping_rate_factor * temperature_factor
+    
+    def concentration_factor(self, final_volume, boil_volume):
+        """
+        Calculate the concentration factor to account for extract brews.
+        """
+        return final_volume / boil_volume
+    
+    def boil_gravity(self, concentration_factor, starting_gravity):
+        """
+        Calculate boil gravity.
+        """
+        return (concentration_factor * (starting_gravity -1)) + 1
+    
+    def gravity_factor(self, boil_gravity):
+        """
+        Calculate gravity factor.
+        """
+        return (boil_gravity - 1.050 / 0.2) + 1
+    
+    def hopping_rate_factor(self, concentration_factor, desired_ibu):
+        """
+        Calculate hopping rate factor.
+        """
+        return ((concentration_factor * desired_ibu) / 260) + 1
+        
+    def temperature_factor(self, elevation_in_feet):
+        """
+        Calculate temperature factor
+        """
+        return ((elevation_in_feet / 550) * 0.02) + 1
+    
+    
