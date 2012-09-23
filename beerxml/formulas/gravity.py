9e5561a6a1ef3e58e9b3694647c3f0e8beb15c72
Rolf Håvard Blindheim <rhblind@gmail.com>
2012-09-23 20:57:48 +0200

    added some equations

--- beerxml/formulas/gravity.py
+++ beerxml/formulas/gravity.py
@@ -2,37 +2,60 @@
 #
 # References:
 # http://hbd.org/ensmingr/
-# http://plato.montanahomebrewers.org/
+# http://en.wikipedia.org/wiki/Brix
 # http://en.wikipedia.org/wiki/Gravity_(alcoholic_beverage)
 # http://morebeer.com/brewingtechniques/library/backissues/issue2.1/manning.html
 
 def plato_to_gravity(degrees_plato):
     """
-    Convert degrees plato to specific gravity.
+    Convert degrees plato to gravity.
     """
     return 259.0 / (259.0 - degrees_plato)
 
-def gravity_to_plato(specific_gravity):
+def gravity_to_plato(gravity):
     """
-    Convert specific gravity to degrees plato.
+    Convert gravity to degrees plato.
     """
-    return 259.0 - (259.0 / specific_gravity)
+    return 259.0 - (259.0 / gravity)
+
+def gravity_to_brix(gravity):
+    """
+    Degrees Brix (symbol °Bx) is the sugar content of an aqueous solution.
+    """
+    return (((182.4601 * gravity -775.6821) * gravity \
+             + 1262.7794) * gravity - 669.5622)
     
+def brix_to_gravity(brix):
+    """
+    TODO: implement
+    """
+    pass
+
 def specific_gravity(original_gravity, final_gravity):
     """
     The percentage of alcohol can be calculated from the 
     difference between the original gravity of the wort 
     and the current specific gravity of wort.
     """
-    pass
+    return ((1.05 * (original_gravity - final_gravity) / final_gravity) / 0.79)
 
-def original_extract():
-    pass
+def alcohol_by_volume(original_gravity, final_gravity):
+    """
+    Calculate the Alcohol By Volume (ABV).
+    """
+    return (original_gravity - final_gravity) / 0.75
 
-
-def apparent_extract():
-    pass
-
+def alcohol_by_weight(alcohol_by_volume, final_gravity=None):
+    """
+    Calculate the Alcohol By Weight (ABW).
+    If the final gravity is not know, but has "normal" levels
+    of alcohol and attenuation, the ABW can be calculated
+    using this formula: (0.78 * alcohol_by_volume)
+    """
+    if final_gravity is not None:
+        return (0.79 * alcohol_by_volume) / final_gravity
+    return (0.78 * alcohol_by_volume)
+    
 def true_extract():
     pass
 
